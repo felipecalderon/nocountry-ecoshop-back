3000166635ac86078336cd25990804f3f8636979
AFacundoOrtiz <urban12the@gmail.com>
2025-11-20 10:23:58 -0300

    feat(auth): dependencies problem fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,14 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [
+    ConfigModule,
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
