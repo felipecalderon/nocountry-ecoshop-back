063f9281e143f73f83689760fae252082421d188
AFacundoOrtiz <urban12the@gmail.com>
2025-11-24 22:29:12 -0300

    feat(auth): implement RolesGuard and Roles decorator for RBAC

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRole } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('admin-only')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  getAdminData() {
+    return {
+      message: 'Si ves esto, las guards de admin funcionan xd',
+    };
+  }
 }
