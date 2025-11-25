344e382975719dba7e32805538982da337d6bbb5
Felipe <felipe.calderon321@gmail.com>
2025-11-25 09:49:54 -0300

    fix: modularized swagger config

---
+++ src/swagger.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const swaggerConfig = (app: INestApplication<any>) => {
+  const config = new DocumentBuilder()
+    .setTitle('EcoShop E-commerce')
+    .setDescription(
+      'API de EcoShop para la gestion de productos sostenibles y ecologicos',
+    )
+    .setVersion('1.0')
+    .addTag('products', 'operaciones relacionadas con productos')
+    .addTag('certifications', 'operaciones de certificaciones')
+    .addTag('orders', 'gestion de ordenes')
+    .addTag('users', 'gestion de usuarios')
+    .addTag('brands', 'gestion de marcas')
+    // .addBearerAuth() //por si usamos auth de JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    jsonDocumentUrl: 'api/docs-json',
+  });
+};
