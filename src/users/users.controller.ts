39c7a92518cc9bd9ef8e94f0acb7b84382d9600e
AFacundoOrtiz <urban12the@gmail.com>
2025-11-26 11:36:07 -0300

    feat: implement user profile update and address management
    Feat: Added PATCH /users/profile for secure user updates. Feat: Implemented Address entity, service, and controller with CRUD operations. Security: Enforced ownership validation (Anti-IDOR) using GetUser decorator.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,55 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
-import { UserRole } from './entities/user.entity';
+import { User, UserRole } from './entities/user.entity';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+@ApiTags('Usuarios')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('admin-only')
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Endpoint de prueba para probar Guard de Roles.' })
+  @UseGuards(RolesGuard)
   @Roles(UserRole.ADMIN)
   getAdminData() {
     return {
       message: 'Si ves esto, las guards de admin funcionan xd',
     };
+  }
+
+  @Get('profile')
+  @ApiOperation({ summary: 'Obtener perfil del usuario autenticado' })
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna los datos del usuario logueado.',
+  })
+  getProfile(@GetUser() user: User) {
+    return this.usersService.findOne(user.id);
+  }
+
+  @Patch('profile')
+  @ApiOperation({ summary: 'Actualizar perfil del usuario autenticado' })
+  @ApiResponse({
+    status: 200,
+    description: 'Perfil actualizado correctamente.',
+  })
+  updateProfile(
+    @GetUser('id') userId: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(userId, updateUserDto);
   }
 }
