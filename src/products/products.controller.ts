ade894cfcf76b221be5398b1efb217c502e7c868
AFacundoOrtiz <urban12the@gmail.com>
2025-12-01 13:05:40 -0300

    MaterialComposition CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRole } from 'src/users/entities/user.entity';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { CreateMaterialCompositionDto } from './dto/material-composition.dto';
+import { UpdateMaterialCompositionDto } from './dto/update-material-composition.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Crear nuevo material base' })
+  create(@Body() createDto: CreateMaterialCompositionDto) {
+    return this.productsService.create(createDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Listar todos los materiales' })
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Obtener un material por ID' })
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Actualizar material' })
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateDto: UpdateMaterialCompositionDto,
+  ) {
+    return this.productsService.update(id, updateDto);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Eliminar material' })
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.remove(id);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MaterialComposition } from './entities/material-composition.entity';
+import { Repository } from 'typeorm';
+import { CreateMaterialCompositionDto } from './dto/material-composition.dto';
+import { UpdateMaterialCompositionDto } from './dto/update-material-composition.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(MaterialComposition)
+    private readonly materialRepository: Repository<MaterialComposition>,
+  ) {}
+
+  async create(createMaterialDto: CreateMaterialCompositionDto) {
+    const existing = await this.materialRepository.findOne({
+      where: { name: createMaterialDto.name },
+    });
+
+    if (existing) {
+      throw new BadRequestException('Ya existe un material con este nombre.');
+    }
+
+    const material = this.materialRepository.create(createMaterialDto);
+    return await this.materialRepository.save(material);
+  }
+
+  async findAll() {
+    return await this.materialRepository.find({
+      order: { name: 'ASC' },
+    });
+  }
+
+  async findOne(id: string) {
+    const material = await this.materialRepository.findOne({ where: { id } });
+    if (!material)
+      throw new NotFoundException(`Material con ID ${id} no encontrado`);
+    return material;
+  }
+
+  async update(id: string, updateMaterialDto: UpdateMaterialCompositionDto) {
+    const material = await this.findOne(id);
+
+    this.materialRepository.merge(material, updateMaterialDto);
+
+    return await this.materialRepository.save(material);
+  }
+
+  async remove(id: string) {
+    const material = await this.findOne(id);
+    return await this.materialRepository.remove(material);
+  }
+}
